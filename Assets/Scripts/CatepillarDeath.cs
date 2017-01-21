3b5e2446ddafbb5fd275a64f1851cac4660e60f1
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-01-21 19:40:41 +0000

    Death

---
+++ Assets/Scripts/CatepillarDeath.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatepillarDeath : MonoBehaviour {
+
+	public int player;
+	public List<GameObject> segments = new List<GameObject>();
+
+
+	void OnEnable()
+	{
+		Events.AddListener<PlayerKilled>(OnPlayerKilled);
+	}
+
+	void OnDisable()
+	{
+		Events.RemoveListener<PlayerKilled>(OnPlayerKilled);
+	}
+
+	private void OnPlayerKilled(PlayerKilled _event)
+	{
+		if (player == _event.player)
+		{
+			for (int i = 0; i < segments.Count; ++i)
+			{
+				Joint2D joint = segments[i].GetComponent<Joint2D>();
+				if (joint != null)
+				{
+					joint.enabled = false;
+				}
+				Collider2D collider = segments[i].GetComponent<Collider2D>();
+				if (collider != null)
+				{
+					collider.enabled = false;
+				}
+				Rigidbody2D body = segments[i].GetComponent<Rigidbody2D>();
+				if (body != null)
+				{
+					body.isKinematic = false;
+					body.AddForce(85f*(new Vector2(Random.Range(0f,1f),Random.Range(0f,1f))).normalized);
+				}
+				CaterpillerMovement movement = segments[i].GetComponent<CaterpillerMovement>();
+				if (movement)
+				{
+					movement.enabled = false;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerKilled : GameEvent
+{
+	public int player = 0;
+
+	public PlayerKilled(int _player)
+	{
+		player = _player;
+	}
+}
+
+public class Hazard : MonoBehaviour {
+
+
+	public List<string> playerTags = new List<string>();
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		Debug.Log("Hazard trigger");
+		for (int i = 0; i < playerTags.Count; ++i)
+		{
+			if (otherCollider.gameObject.tag == playerTags[i])
+			{
+				Events.Raise(new PlayerKilled(i));
+				Debug.Log("KILLED PLAYER "+(i+1));
+
+				// TODO: Body parts separate, fly in random directions, fall through floor, destroyed off screen.
+				// TODO: Trigger player flash and death
+				// TODO: Trigger respawn
+			}
+		}
+	}
+}
