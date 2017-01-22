7635a4c2c01e13fc8a21d4b4ded3028b53537d33
Sarah Herzog <sarah.ann.herzog@gmail.com>
2017-01-22 11:46:20 +0000

    Frog and chicks

---
+++ Assets/Scripts/Chick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chick : MonoBehaviour {
+
+	public float CycleOffset = 0;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator>().SetFloat("CycleOffset",CycleOffset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Frog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour {
+
+	public Animator animator;
+
+	public float tongueInterval = 5.0f;
+
+	private float lastTongue = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= lastTongue + tongueInterval)
+		{
+			lastTongue = Time.time;
+			animator.SetTrigger("Tongue");
+		}
+	}
+}
